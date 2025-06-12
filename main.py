2da8d21bb0aa359ccf717779d396e74903d71676
Botirov Baxodir <botirovboha@gmail.com>
2025-06-12 15:46:13 +0500

    Removed flask

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-from flask import Flask
-from threading import Thread
 from bot import run_bot
 import time
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "✅ CS 1.6 Telegram Bot is running!"
-
-def run_flask():
-    app.run(host="0.0.0.0", port=10000)
-
-
 
 if __name__ == "__main__":
-    Thread(target=run_flask, daemon=True).start()
+
     run_bot()
 
    
